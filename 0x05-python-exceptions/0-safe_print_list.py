3ad7e06774228dfa688d6e678447d5fd11b22e4a
“taissir-l” <taissirlassoued1@gmail.com>
2024-01-22 14:08:33 -0800

    task0 corrections

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    summ = 0
-    for i in range(x):
-        try:
-            print(f"{my_list[i]}", end="")
-            summ += 1
-        except IndexError:
-            break
-    print()
-    return(summ)
+     if my_list is None or x < 0:
+         return (0)
+     i = 0
+     try:
+         while i < x:
+             print("{}".format(my_list[i]), end="")
+             i += 1
+     except IndexError:
+         print("")
+         return (i)
+     print("")
+     return (i)
