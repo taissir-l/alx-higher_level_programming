3857bba0364b634ac9ccabf1299be3817d4930fd
“taissir-l” <taissirlassoued1@gmail.com>
2024-01-22 14:54:00 -0800

    advanced tasks

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import sys
 def safe_print_integer_err(value):
-    ry:
+    try:
         print("{:d}".format(value))
         return (True)
     except (TypeError, ValueError):
