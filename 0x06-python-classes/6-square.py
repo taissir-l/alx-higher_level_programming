0114fd7bc0d9a41457267c16c96507c98da13a44
“taissir-l” <taissirlassoued1@gmail.com>
2024-01-23 14:46:50 -0800

    task6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -47,13 +47,11 @@ class Square:
         """current area"""
         return (self.__size * self.__size)
 
-
     def my_print(self):
         """Print square with # character."""
         if self.__size == 0:
             print("")
             return
-
 
         [print("") for i in range(0, self.__position[1])]
         for i in range(0, self.__size):
