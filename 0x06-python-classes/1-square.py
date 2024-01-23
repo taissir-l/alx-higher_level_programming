f3fc48309424734cdc1e3030d02dbabe546bd721
“taissir-l” <taissirlassoued1@gmail.com>
2024-01-23 12:12:10 -0800

    task1 fix

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+"""defines a quare and set the size as private instance variable"""
+
 class Square:
+    """class to create square."""
     def __init__(self, size):
+        """the Initialzation of the size to piV
+
+        Args:
+            size (int): the square size
+        """
+
         self.__size = size
