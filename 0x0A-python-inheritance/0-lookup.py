693fb45601ee4d41afaf76c191b7bef23e2712a8
“taissir-l” <taissirlassoued1@gmail.com>
2024-02-05 12:26:33 -0800

    task0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+   Module returns the list of available attributes and methods of an object
+"""
+
+
+def lookup(obj):
+    """function to look for attributes and methods of an object"""
+    return dir(obj)
