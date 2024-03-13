986f248f1d875834c130aaa550b21e106f4c0a3e
“taissir-l” <taissirlassoued1@gmail.com>
2024-03-12 17:08:34 -0700

    mysql

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,6 @@
+-- Import in hbtn_0c_0 database this table dump
+--  script that displays the average temperature (Fahrenheit)
+-- by city ordered by temperature (descending).
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE hbtn_0c_0;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
