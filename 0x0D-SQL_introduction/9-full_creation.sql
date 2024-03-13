5da7c9c6634d3fb1e829143d7487aece5305c37f
“taissir-l” <taissirlassoued1@gmail.com>
2024-03-12 17:16:00 -0700

    fix 9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,10 +1,7 @@
 -- a script that creates a table second_table in the database
 -- btn_0c_0 in your MySQL server and add multiples rows.
-CREATE TABLE IF NOT EXISTS second_table (id INT,
-name VARCHAR(256),
-score INT);
-gNSERT INTO second_table (id, name, score)
-VALUES (1, "John", 10),
-(2, "Alex", 3),
-(3, "Bob", 14),
-(4, "George", 8);
+CREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256), `score` INT);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES(1, 'John', 10);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES(2, 'Alex', 3);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES(3, 'Bob', 14);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES(4, 'George', 8);
