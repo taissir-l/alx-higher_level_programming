d805e7e4b00b57b6f3a0f1312cf3fd32668b6ba8
“taissir-l” <taissirlassoued1@gmail.com>
2024-04-14 07:54:08 -0700

    fixing 2and 6

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,14 +1,25 @@
 #!/usr/bin/python3
-"""to select all states from the database hbtn_0e_0_usa"""
-import MySQLdb
+''' selects rows with a name that matches the given argument'''
 import sys
+import MySQLdb
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    cur = db.cursor()
-    cur.execute("SELECT * FROM `states` WHERE name='{:s}'\
-            ORDER BY `id` ASC".format(sys.argv[4]))
-    for a in cur.fetchall():
-        print(a)
-    cur.close()
-    db.close()
+
+if __name__ == '__main__':
+    if len(sys.argv) >= 5:
+        db_connection = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
+        )
+        cursor = db_connection.cursor()
+        state_name = sys.argv[4]
+        cursor.execute(
+            'SELECT * FROM states WHERE CAST(name AS BINARY) LIKE ' +
+            'CAST("{}" AS BINARY) ORDER BY id ASC;'.format(state_name)
+        )
+        a = cursor.fetchall()
+        for i in a:
+            print(i)
+        db_connection.close()
